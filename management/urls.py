1cc24543e4d049ecb3f8e04b833f7d9dcb9a472e
lilianwaweru <lilowesh.lw@gmail.com>
2020-02-05 16:45:03 +0300

    added carousel

--- management/urls.py
+++ management/urls.py
@@ -20,6 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("staff.urls")),
-    path(r'accounts/', include('django.contrib.auth.urls')),
-   
+       
 ]
