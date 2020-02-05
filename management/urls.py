bcb8e2b5aa52af86a9c24dc8f317ec00b0e16f40
lilianwaweru <lilowesh.lw@gmail.com>
2020-02-05 09:51:26 +0300

    created a navbar

--- management/urls.py
+++ management/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("staff.urls"))
+    path('',include("staff.urls")),
+    path(r'accounts/', include('django.contrib.auth.urls')),
+   
 ]
---
+++ staff/static/js/staff.js
@@ -0,0 +1,17 @@
+// $(document).ready(function() {
+//     // SideNav Button Initialization
+//     $(".button-collapse").sideNav();
+//       // SideNav Scrollbar Initialization
+//       var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+//       var ps = new PerfectScrollbar(sideNavScrollbar);
+//   });
+
+  /* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
